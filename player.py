90939f763a93a27ab783e053c53e1724dd1d2397
rohitn01 <rohitmen01@gmail.com>
2020-05-06 01:38:00 -0400

    made player class

---
+++ player.py
@@ -0,0 +1,22 @@
+class Player:
+    def __init__(self, name, p1, p2):
+        self.name = name
+        self.p1 = p1
+        self.p2 = p2
+
+    def getName(player):
+        return player.name
+    def getPosOne(player):
+        return player.p1
+    def getPosTwo(player):
+        return player.p2
+
+    def setName(player, name):
+        player.name = name
+    def setPosOne(player, p1):
+        player.p1 = p1
+    def setPosTwo(player, p2):
+        player.p2 = p2
+
+    
+        
